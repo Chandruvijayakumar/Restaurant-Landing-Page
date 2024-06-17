9e46551310e7406aea9125fe1842d84ea7dae7bc
CHANDRU VIJAYAKUMAR <chandru4395393@gmail.com>
2024-06-17 10:48:52 +0530

    First Commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-container">
+          {/* <img src={Logo} alt="" /> */}
+          <h1>Pachayapa's Mess</h1>
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Qualtiy</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Carrers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>6381951562</span>
+          <span>pachayappamess@gmail.com</span>
+          <span>chandru4395393@gmail.com</span>
+          <span>9750744831</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
